5cbda99f716917781975061b05376eea2a2b89d0
Andrés Felipe <andres.cardozo96@gmail.com>
2021-05-15 12:59:38 -0500

    creando seeder de categorias

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    use HasFactory;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory()
+            ->count(50)
+            ->create();
+    }
+}
