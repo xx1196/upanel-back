16336d11f376c312cafb4c3475ff6a9c3efa03df
Andrés Felipe <andres.cardozo96@gmail.com>
2021-07-28 14:52:12 +0000

    Feature/2021 07 10 add social login google

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Joselfonseca\LighthouseGraphQLPassport\HasSocialLogin;
+use Joselfonseca\LighthouseGraphQLPassport\Models\SocialProvider;
 use Laravel\Passport\HasApiTokens;
+use Laravel\Socialite\Facades\Socialite;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens, HasRoles;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles, HasSocialLogin;
 
     /**
      * The attributes that are mass assignable.
@@ -19,6 +29,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'avatar',
         'email',
         'password',
     ];
@@ -41,4 +52,45 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function socialProviders(): HasMany
+    {
+        return $this->hasMany(SocialProvider::class);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public static function byOAuthToken(Request $request)
+    {
+        $userData = Socialite::driver($request->get('provider'))->userFromToken($request->get('token'));
+
+        try {
+            $user = static::whereHas('socialProviders', function ($query) use ($request, $userData) {
+                $query->where('provider', Str::lower($request->get('provider')))->where('provider_id', $userData->getId());
+            })->firstOrFail();
+        } catch (ModelNotFoundException $e) {
+            $user = static::where('email', $userData->getEmail())->first();
+            if (!$user) {
+                $user = static::create([
+                    'name' => $userData->getName(),
+                    'email' => $userData->getEmail(),
+                    'avatar' => $userData->getAvatar(),
+                    'uuid' => Str::uuid(),
+                    'password' => Hash::make(Str::random(16)),
+                    'email_verified_at' => now(),
+                ]);
+            }
+            SocialProvider::create([
+                'user_id' => $user->id,
+                'provider' => $request->get('provider'),
+                'provider_id' => $userData->getId(),
+            ]);
+        }
+        Auth::setUser($user);
+
+        return $user;
+    }
 }
